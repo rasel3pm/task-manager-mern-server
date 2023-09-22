b94cb4d5266b1023a3c2c0a60fdc8ef407a16d19
Rasel <mdraselkibria11@gmail.com>
2023-09-23 02:13:41 +0600

    routing endpoint change

--- src/router/api.js
+++ src/router/api.js
@@ -1,19 +1,29 @@
-const { CreateTask, DeleteTask, UpdateTask, ListTaskByStatus,TaskStatusCount } = require("../controller/TasksController")
-const { register, login, ProfileUpdate, GetProfile } = require("../controller/UserController")
-const AuthVerification = require("../middleware/AuthVerification")
+const {
+  CreateTask,
+  DeleteTask,
+  UpdateTask,
+  ListTaskByStatus,
+  TaskStatusCount,
+} = require("../controller/TasksController");
+const {
+  register,
+  login,
+  ProfileUpdate,
+  GetProfile,
+} = require("../controller/UserController");
+const AuthVerification = require("../middleware/AuthVerification");
 
-const router = require("express").Router()
+const router = require("express").Router();
 
+router.post("/registration", register);
+router.post("/login", login);
+router.post("/ProfileUpdate", AuthVerification, ProfileUpdate);
+router.get("/GetProfile", AuthVerification, GetProfile);
 
-router.post("/registration",register)
-router.post("/login",login)
-router.post("/ProfileUpdate", AuthVerification,ProfileUpdate)
-router.get("/GetProfile",AuthVerification,GetProfile)
+router.post("/CreateTask", AuthVerification, CreateTask);
+router.get("/DeleteTask/:id", AuthVerification, DeleteTask);
+router.get("/UpdateTask/:id/:status", AuthVerification, UpdateTask);
+router.get("/ListTaskByStatus/:status", AuthVerification, ListTaskByStatus);
+router.get("/TaskStatusCount", AuthVerification, TaskStatusCount);
 
-router.post("/CreateTask",AuthVerification,CreateTask)
-router.post("/DeleteTask/:id",AuthVerification,DeleteTask)
-router.get("/UpdateTask/:id/:status",AuthVerification,UpdateTask)
-router.get("/ListTaskByStatus/:status",AuthVerification,ListTaskByStatus)
-router.get("/TaskStatusCount",AuthVerification,TaskStatusCount)
-
-module.exports=router
+module.exports = router;
